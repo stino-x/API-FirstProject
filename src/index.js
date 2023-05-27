54e120a365db13286dfad504f595b0a3aa8f1ad4
stino-x <107426247+stino-x@users.noreply.github.com>
2023-05-27 16:41:07 +0200

    project completion

--- src/index.js
+++ src/index.js
@@ -1,33 +1,65 @@
-import './style.css';
+import Updateposition from './Modules/input.js';
 
-const activityList = document.querySelector('#activity-list');
-const activityArray = [{
-  description: 'Go to the gym',
-  completed: false,
-  Index: 0,
-}, {
-  description: 'Study',
-  completed: true,
-  Index: 1,
-}, {
-  description: 'Cook',
-  completed: false,
-  Index: 2,
-}, {
-  description: 'Suceed',
-  completed: true,
-  Index: 3,
-}, {
-  description: 'Travel',
-  completed: false,
-  Index: 4,
-}];
-window.addEventListener('load', () => {
-  let content = '';
-  activityArray.forEach((activity, index) => {
-    content += `<li class="section" id="${index}">
-        <span class="activity td"><input type="checkbox" name="" class="check-box" ${activity.completed ? 'checked' : ''}><span>${activity.description}</span><i class="fa-solid fa-ellipsis-vertical"></i></span>
-    </li>`;
-    activityList.innerHTML = content;
+const refreshButton = document.querySelector('.refresh');
+const submitButton = document.querySelector('#submit');
+const namevalue = document.querySelector('#name');
+const score = document.querySelector('#score');
+const scoreList = document.querySelector('#scores');
+const Form = document.querySelector('form');
+const displayError = (message) => {
+  const container = document.querySelector('.container-2');
+  const errorMessage = document.createElement('div');
+  errorMessage.setAttribute('class', 'error-message');
+  errorMessage.textContent = message;
+  container.appendChild = errorMessage;
+};
+const newCompetitor = (data) => {
+  while (scoreList.firstChild) {
+    scoreList.removeChild(scoreList.firstChild);
+  }
+  data.forEach((person) => {
+    const contestant = document.createElement('li');
+    const contestantID = data.indexOf(person) + 1;
+    contestant.setAttribute('id', `${contestantID}`);
+    contestant.innerHTML = `${person.user}:${person.score}`;
+    scoreList.appendChild(contestant);
   });
-});
+};
+const submitScore = (e) => {
+  e.preventDefault();
+  const updatePosition = new Updateposition(namevalue.value, score.value);
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/austin/scores/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(updatePosition),
+  })
+    .then((response) => {
+      Form.addEventListener('submit', (event) => {
+        // Prevent the default form submission behavior
+        event.preventDefault();
+
+        // Check if the form is valid
+        if (!response.ok) {
+          // If the form is not valid, display an error message
+          displayError('Leaderboard score was not created correctly');
+        } else {
+          // If the form is valid, submit it
+          Form.reset();
+        }
+      });
+    });
+  Form.reset();
+};
+const displayScore = () => {
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/austin/scores/')
+    .then((response) => response.json())
+    .then((data) => {
+      newCompetitor(data.result);
+    });
+};
+
+submitButton.addEventListener('click', submitScore);
+refreshButton.addEventListener('click', displayScore);
+displayScore();
